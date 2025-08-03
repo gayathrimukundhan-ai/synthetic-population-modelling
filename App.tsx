9c3fe3c1ce78fa0393e48834089b5305f0043115
gayathrimukundhan-ai <gayathri.mukundhan@gmail.com>
2025-08-03 19:37:57 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,1 @@
-import React from 'react';
-import PharmacyGrocerDashboard from './components/PharmacyGrocerDashboard';
 
-const App: React.FC = () => {
-  return <PharmacyGrocerDashboard />;
-};
-
-export default App;
